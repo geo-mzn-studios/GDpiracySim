afbfa7e6d3d3afa7b6e7e12b32d66a5d82a08309
geo-studios-official <geo.studios.official@gmail.com>
2026-05-07 06:33:29 -0700

    Add files via upload

---
+++ windows/src/main.cpp
@@ -0,0 +1,25 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/utils/web.hpp>
+
+using namespace geode::prelude;
+
+$on_mod(Loaded) {
+    geode::log::info("Hello World!");
+
+    if (Mod::get()->getSettingValue<bool>("pirate")) {
+    geode::createQuickPopup(
+    "Pirate detected!",
+    "<cy>Online features</c> have been <cr>disabled</c>. Please consider purchasing the <cg>full version</c> as it really helps us to continue developing <cl>Geometry Dash</c>!",
+    "OK :(",
+    "Get Full",
+    [](FLAlertLayer* layer, bool btn2) {
+        if (btn2) {
+            geode::utils::web::openLinkInBrowser(
+                "https://store.steampowered.com/app/322170/Geometry_Dash"
+            );
+        }
+    }
+    );
+    }
+}

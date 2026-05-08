887858acdb6dab4b58bb88e0b2979b6e70f889b7
geo-studios-official <geo.studios.official@gmail.com>
2026-05-08 02:29:18 -0700

    EMERGENCY UPDATE CODE

--- android64/src/main.cpp
+++ android64/src/main.cpp
@@ -4,22 +4,27 @@
 
 using namespace geode::prelude;
 
-$on_mod(Loaded) {
-    geode::log::info("Hello World!");
+class $modify(MyMenuLayer, MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init())
+            return false;
 
-    if (Mod::get()->getSettingValue<bool>("pirate")) {
-    geode::createQuickPopup(
-    "Pirate detected!",
-    "<cy>Online features</c> have been <cr>disabled</c>. Please consider purchasing the <cg>full version</c> as it really helps us to continue developing <cl>Geometry Dash</c>!",
-    "OK :(",
-    "Get Full",
-    [](FLAlertLayer* layer, bool btn2) {
-        if (btn2) {
-            geode::utils::web::openLinkInBrowser(
-                "https://play.google.com/store/apps/details?id=com.robtopx.geometryjump"
+        if (Mod::get()->getSettingValue<bool>("pirate")) {
+            geode::createQuickPopup(
+                "Pirate detected!",
+                "<cy>Online features</c> have been <cr>disabled</c>. Please consider purchasing the <cg>full version</c> as it really helps us to continue developing <cl>Geometry Dash</c>!",
+                "OK :(",
+                "Get Full",
+                [](FLAlertLayer*, bool btn2) {
+                    if (btn2) {
+                        geode::utils::web::openLinkInBrowser(
+                            "https://play.google.com/store/apps/details?id=com.robtopx.geometryjump"
+                        );
+                    }
+                }
             );
         }
+
+        return true;
     }
-    );
-    }
-}
+};

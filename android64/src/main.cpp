e222b67928f517c0a19356ea0e9ed383d28819ef
geo-studios-official <geo.studios.official@gmail.com>
2026-05-09 08:35:49 +0800

    New stable code

--- android64/src/main.cpp
+++ android64/src/main.cpp
@@ -5,26 +5,41 @@
 using namespace geode::prelude;
 
 class $modify(MyMenuLayer, MenuLayer) {
+    static bool s_shown;
+
     bool init() {
         if (!MenuLayer::init())
             return false;
 
-        if (Mod::get()->getSettingValue<bool>("pirate")) {
-            geode::createQuickPopup(
-                "Pirate detected!",
-                "<cy>Online features</c> have been <cr>disabled</c>. Please consider purchasing the <cg>full version</c> as it really helps us to continue developing <cl>Geometry Dash</c>!",
-                "OK :(",
-                "Get Full",
-                [](FLAlertLayer*, bool btn2) {
-                    if (btn2) {
-                        geode::utils::web::openLinkInBrowser(
-                            "https://play.google.com/store/apps/details?id=com.robtopx.geometryjump"
-                        );
-                    }
-                }
-            );
+        if (Mod::get()->getSettingValue<bool>("pirate") && !s_shown) {
+            s_shown = true;
+
+            // wait until menu is fully loaded
+            this->runAction(CCSequence::create(
+                CCDelayTime::create(0.3f),
+                CCCallFunc::create(this, callfunc_selector(MyMenuLayer::showPiratePopup)),
+                nullptr
+            ));
         }
 
         return true;
     }
+
+    void showPiratePopup() {
+        geode::createQuickPopup(
+            "Pirate detected!",
+            "<cy>Online features</c> have been <cr>disabled</c>. Please consider purchasing the <cg>full version</c> as it really helps us to continue developing <cl>Geometry Dash</c>!",
+            "OK :(",
+            "Get Full",
+            [](FLAlertLayer*, bool btn2) {
+                if (btn2) {
+                    geode::utils::web::openLinkInBrowser(
+                        "https://play.google.com/store/apps/details?id=com.robtopx.geometryjump"
+                    );
+                }
+            }
+        );
+    }
 };
+
+bool MyMenuLayer::s_shown = false;
